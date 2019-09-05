22e1b3146982f003230d5147e0f976d5e7eb320c
Thomas Charlat <thomas.charlat@gmail.com>
2019-09-05 14:52:02 +0200

    feat(prismic): tuto implementation

--- prismic/react/src/App.js
+++ prismic/react/src/App.js
@@ -1,20 +1,34 @@
-import React from 'react';
+import React from "react";
 import {
   BrowserRouter as Router,
   Route,
   Switch,
-  Redirect,
-} from 'react-router-dom';
-import Preview from './Preview';
-import Help from './Help';
-import NotFound from './NotFound';
+  Redirect
+} from "react-router-dom";
+import Preview from "./Preview";
+import Page from "./Page";
+import Help from "./Help";
+import NotFound from "./NotFound";
 
-const App = (props) => (
+const App = props => (
   <Router>
     <Switch>
-      <Redirect exact from="/" to="/help"/>
+      <Redirect exact from="/" to="/help" />
       <Route exact path="/help" component={Help} />
-      <Route exact path="/preview" render={routeProps => <Preview {...routeProps} prismicCtx={props.prismicCtx} />} />
+      <Route
+        exact
+        path="/preview"
+        render={routeProps => (
+          <Preview {...routeProps} prismicCtx={props.prismicCtx} />
+        )}
+      />
+      <Route
+        exact
+        path="/page/:uid"
+        render={routeProps => (
+          <Page {...routeProps} prismicCtx={props.prismicCtx} />
+        )}
+      />
       <Route component={NotFound} />
     </Switch>
   </Router>
